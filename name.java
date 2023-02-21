5568b8388398c2f1741d271af818a1ffa9d0bd48
Anubhab <anubhabchowdhury01@gmail.com>
2023-02-21 20:25:28 +0530

    Entering Name from user

---
+++ name.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+class name{
+	public static void main( String arg[]){
+		String name;
+		System.out.println("May I know your Name");
+		String n;
+		Scanner s= new Scanner(System.in);
+		n=s.nextLine();
+		System.out.println("Your name is " + n);
+	}
+}
