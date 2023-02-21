d15ea2d60e6e9bc696002e81e3cc663dfe4e2289
Anubhab <anubhabchowdhury01@gmail.com>
2023-02-21 13:02:00 +0530

    Addition of two number

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class add{
+	public static void main(String arg[]){
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter two no");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Sum is "+ c);
+	}
+}
+
