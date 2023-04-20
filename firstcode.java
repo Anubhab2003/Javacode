4b6997889cf6f244d440e47123da00521f18157e
Anubhab <anubhabchowdhury01@gmail.com>
2023-04-21 00:03:47 +0530

    A bit more optimize code

--- firstcode.java
+++ firstcode.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 class Myfirst{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World Anubhab");
 	}
 }
--- prime.java
+++ prime.java
@@ -1,25 +1,26 @@
 import java.util.*;
-public class prime{
-	public static void main(String args[]){
-		Scanner s= new Scanner(System.in);
-		System.out.print("Enter a number Sir :");
-		n=s.nextInt();
-		if(n==2){
-			System.out.println("Yes its prime number");
-		}else{
-			boolean isprime= true;
-			for(int i=2;i<=n-1;i++){
-				if (i%n==0){
-					isprime= false;
-				}
-			if (isprime==true){
-				System.out.println("Yes the given number is prime");
-			}else{
-				System.out.println("The number is composite");
-			}
-		}
-	}
+
+public class prime {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = s.nextInt();
+        if (n == 2) {
+            System.out.println("Yes, it's a prime number");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime == true) {
+                System.out.println("Yes, the given number is prime");
+            } else {
+                System.out.println("The number is composite");
+            }
+        }
+    }
 }
-
-
 
