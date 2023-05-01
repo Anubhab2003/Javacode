a48452ba74bde2e9972a2c4857db3b35dc0bd715
root <root@Anubhab.localdomain>
2023-05-01 21:08:53 +0530

    Simple code of input of 3 sub mark

---
+++ array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class array{
+	public static void main(String args []){
+		Scanner s=new Scanner(System.in);
+		int mark[]=new int[3];
+		mark[0]=s.nextInt();
+		mark[1]=s.nextInt();
+		mark[2]=s.nextInt();
+		System.out.println("phy"+mark[0]);
+		System.out.println("maths"+mark[1]);
+		System.out.println("chem"+mark[2]);
+	}
+}
