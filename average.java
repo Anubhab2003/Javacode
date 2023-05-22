c3ed62d79afd2c3120c091e8819444a3bea3c3e0
Anubhab <anubhabchowdhury01@gmail.com>
2023-05-22 12:17:49 +0530

    Average

---
+++ average.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class average{
+	public static void main(String args[]){
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the number of total number entered");
+		int n=s.nextInt();
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		System.out.println((a+b+c)/n);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
